eb9cca57d00a8b488f65b8bef392022771a9d603
Hermann EHO <hermannleboss@gmail.com>
2023-11-16 19:36:38 +0000

    feat: home team + timeline

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import HeroComponent from '@/components/HeroComponent.vue'
+import TeamComponent from '@/components/TeamComponent.vue'
+import TimelineComponent from '@/components/TimelineComponent.vue'
 </script>
 
 <template>
   <main>
     <HeroComponent />
+    <TeamComponent />
+    <TimelineComponent />
   </main>
 </template>
+
+<style lang="scss"></style>
