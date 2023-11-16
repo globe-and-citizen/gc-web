2dd8ba9d66a6683a79efb27c07e759c078b5efef
Hermann EHO <hermannleboss@gmail.com>
2023-11-16 09:30:30 +0000

    feat: nav bar component

---
+++ src/components/NavBarComponent.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div class="navbar bg-base-100">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">Globe & Citizen</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li><a>Link</a></li>
+        <li><a>Link 1</a></li>
+        <li><a>Link 2</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
