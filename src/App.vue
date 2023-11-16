fe1a7528f622884956b5bcc32f985e024c3bf6ff
Hermann EHO <hermannleboss@gmail.com>
2023-11-16 09:37:26 +0000

    feat: clean code

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,6 @@ import NavBarComponent from '@/components/NavBarComponent.vue'
   <main class="h-full flex flex-col justify-between">
     <div>
       <NavBarComponent></NavBarComponent>
-      <header>
-        <div class="wrapper">
-          <nav>
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/about">About</RouterLink>
-          </nav>
-        </div>
-      </header>
-
       <RouterView />
     </div>
     <FooterComponent></FooterComponent>
