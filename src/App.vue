8d82660ae9b263ff2540c3bd729197c92c892e2b
Hermann EHO <hermannleboss@gmail.com>
2023-11-16 09:30:45 +0000

    feat: use of nav bar component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import FooterComponent from '@/components/FooterComponent.vue'
+import NavBarComponent from '@/components/NavBarComponent.vue'
 </script>
 
 <template>
   <main class="h-full flex flex-col justify-between">
     <div>
+      <NavBarComponent></NavBarComponent>
       <header>
         <div class="wrapper">
           <nav>
