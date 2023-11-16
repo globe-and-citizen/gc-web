adba742fbf95195a7ab76f68236aec03ff7efbe0
Hermann EHO <hermannleboss@gmail.com>
2023-11-16 19:36:22 +0000

    feat: refactoring of hero component

--- src/components/HeroComponent.vue
+++ src/components/HeroComponent.vue
@@ -1,19 +1,14 @@
 <template>
-  <div class='hero bg-base-200 py-12'>
-    <div class='hero-content text-center'>
-      <div class='max-w-md'>
-        <h1 class='text-5xl font-bold'>The adventure start here</h1>
-        <p class='py-6'>
+  <div class="hero bg-base-200 py-12">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">The adventure start here</h1>
+        <p class="py-6">
           Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
           exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
         </p>
-        <button class='btn btn-primary'>Get Started</button>
+        <button class="btn btn-primary">Get Started</button>
       </div>
     </div>
   </div>
 </template>
-<script>
-export default {
-  name: 'HeroComponent'
-}
-</script>
