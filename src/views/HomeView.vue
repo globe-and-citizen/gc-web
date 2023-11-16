4938787d6b63e05b7fb6ebf1e2c0b0f89f93d876
Hermann EHO <hermannleboss@gmail.com>
2023-11-16 09:37:50 +0000

    feat: format code with prettier

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,8 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-
-    <h1 class="text-3xl font-bold underline text-blue-300">
-      Hello world!
-    </h1>
+    <h1 class="text-3xl font-bold underline text-blue-300">Hello world!</h1>
     <button class="btn">Button</button>
     <button class="btn btn-primary">Button</button>
   </main>
