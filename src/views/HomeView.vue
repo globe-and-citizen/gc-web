0896af559067b5509a84900f7a5acb4fbc2b1e85
Hermann EHO <hermannleboss@gmail.com>
2023-11-16 09:05:12 +0000

    feat: try daisy ui

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,5 +7,7 @@
     <h1 class="text-3xl font-bold underline text-blue-300">
       Hello world!
     </h1>
+    <button class="btn">Button</button>
+    <button class="btn btn-primary">Button</button>
   </main>
 </template>
