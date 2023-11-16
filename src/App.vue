158f1e0a04695cfddf3ad52b8f414c315eb1cf36
Hermann EHO <hermannleboss@gmail.com>
2023-11-16 09:23:54 +0000

    feat: structure app

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import FooterComponent from '@/components/FooterComponent.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <main class="h-full flex flex-col justify-between">
+    <div>
+      <header>
+        <div class="wrapper">
+          <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+          </nav>
+        </div>
+      </header>
 
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <RouterView />
     </div>
-  </header>
-
-  <RouterView />
+    <FooterComponent></FooterComponent>
+  </main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
