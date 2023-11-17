c706b6ad6dcc174a03d6106aa86e8c00ee48dec8
Hermann EHO <hermannleboss@gmail.com>
2023-11-17 02:04:31 +0000

    feat: About component

---
+++ src/components/AboutComponent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="hero min-h-screen">
+    <div class="hero-content flex-col lg:flex-row">
+      <img src="/pexels-johannes-plenio.jpg" class="max-w-sm rounded-lg shadow-2xl" />
+      <div>
+        <h1 class="text-5xl font-bold">Box Office News!</h1>
+        <p class="py-6">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+          exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+        </p>
+        <button class="btn btn-primary">Get Started</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,13 @@ import HeroComponent from '@/components/HeroComponent.vue'
 import TeamComponent from '@/components/TeamComponent.vue'
 import TimelineComponent from '@/components/TimelineComponent.vue'
 import ProjectComponent from '@/components/ProjectComponent.vue'
+import AboutComponent from '@/components/AboutComponent.vue'
 </script>
 
 <template>
   <main>
     <HeroComponent />
+    <AboutComponent />
     <TeamComponent />
     <TimelineComponent />
     <ProjectComponent />
