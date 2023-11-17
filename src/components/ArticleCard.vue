a657fd1b72e9ed63826888a9dc62cd4ff0a2ce08
Hermann EHO <hermannleboss@gmail.com>
2023-11-17 12:10:40 +0000

    feat: style article card

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="p-4 md:w-1/3">
-    <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+    <div
+      class="h-full border border-black rounded-lg overflow-hidden"
+      style="box-shadow: 2px 2px 0 0 #000"
+    >
       <img
         class="lg:h-48 md:h-36 w-full object-cover object-center"
         :src="article.image"
@@ -10,7 +13,7 @@
         <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
           {{ article.category }}
         </h2>
-        <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
+        <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{ article.title }}</h1>
         <p class="leading-relaxed mb-3">
           {{ article.description }}
         </p>
@@ -66,4 +69,17 @@
     </div>
   </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+interface Article {
+  category: string
+  description: string
+  image: string
+  title: string
+}
+
+interface Props {
+  article: Article
+}
+
+defineProps<Props>()
+</script>
