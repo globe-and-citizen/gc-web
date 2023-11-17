b5b3a4b8fc65d3afcfdfc5793306f36344e9af88
Hermann EHO <hermannleboss@gmail.com>
2023-11-17 07:51:21 +0000

    feat: use only team member component for everything

--- src/components/TeamMemberComponent.vue
+++ src/components/TeamMemberComponent.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-    {{}}
     <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
       <img
         alt="team"
@@ -27,5 +26,5 @@ interface Props {
   member: Member
 }
 
-const props = defineProps<Props>()
+defineProps<Props>()
 </script>
