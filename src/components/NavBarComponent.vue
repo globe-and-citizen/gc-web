38cb2850466780854ffdd983de98b6459c04f296
Hermann EHO <hermannleboss@gmail.com>
2023-11-17 11:56:31 +0000

    feat: container for nav bar

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navbar bg-base-100">
+  <div class="container mx-auto navbar bg-base-100">
     <div class="flex-1">
       <a class="btn btn-ghost text-xl">Globe & Citizen</a>
     </div>
