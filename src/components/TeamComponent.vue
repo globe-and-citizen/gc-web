cd5a1291e9ea773d5f27698d0d5ac3c25b9b9255
Hermann EHO <hermannleboss@gmail.com>
2023-11-17 07:48:46 +0000

    feat: team member component

--- src/components/TeamComponent.vue
+++ src/components/TeamComponent.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-const member = {
+import TeamMemberComponent from '@/components/TeamMemberComponent.vue'
+import { ref } from 'vue'
+
+const member = ref({
   name: 'John Doe',
   title: 'Software Engineer',
   image: 'https://dummyimage.com/80x80'
-}
+})
 </script>
 
 <template>
@@ -17,19 +20,8 @@ const member = {
         </p>
       </div>
       <div class="flex flex-wrap -m-2">
-        <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-          <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-            <img
-              alt="team"
-              class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-              :src="member.image"
-            />
-            <div class="flex-grow">
-              <h2 class="text-gray-900 title-font font-medium">{{ member.name }}</h2>
-              <p class="text-gray-500">{{ member.title }}</p>
-            </div>
-          </div>
-        </div>
+        {{ member }}
+        <TeamMemberComponent :member="member" />
         <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
           <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
             <img
---
+++ src/components/TeamMemberComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+    {{}}
+    <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+      <img
+        alt="team"
+        class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+        :src="member.image"
+      />
+      <div class="flex-grow">
+        <h2 class="text-gray-900 title-font font-medium">{{ member.name }}</h2>
+        <p class="text-gray-500">{{ member.title }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+// props are passed in from the parent component
+interface Member {
+  name: string
+  title: string
+  image: string
+}
+
+interface Props {
+  member: Member
+}
+
+const props = defineProps<Props>()
+</script>
