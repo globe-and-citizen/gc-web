1d3a6f0e326db122072c497f66edcf336b770d3f
Hermann EHO <hermannleboss@gmail.com>
2023-11-17 02:19:49 +0000

    feat: style About component

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="hero min-h-screen">
-    <div class="hero-content flex-col lg:flex-row">
-      <img src="/pexels-johannes-plenio.jpg" class="max-w-sm rounded-lg shadow-2xl" />
-      <div>
-        <h1 class="text-5xl font-bold">Box Office News!</h1>
+  <div class="hero py-12">
+    <div class="hero-content flex-col lg:flex-row lg:gap-10">
+      <img
+        src="/pexels-johannes-plenio.jpg"
+        class="flex-1 max-w-md rounded-lg border-black border-solid border-2 w-[500px] h-[500px]"
+        style="box-shadow: 3px 4px 0 0 #000"
+        alt="About image"
+      />
+      <div class="flex-1">
+        <h1 class="text-5xl font-bold">About Us</h1>
         <p class="py-6">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
-          exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+          Globe & Citizen the company is a fully remote company operated from locations spread
+          around the world. Our goal is to innovate and iterate until we are Web3.0’s premier
+          distributed news network. Global issues such as pandemics, supply chain entanglement, and
+          climate change, to name only a few, cannot be solved by any one nation acting alone.
+          Therefore, we have set out to create a global forum that is free from singular regional
+          interests. Leveraging the incredible advances in information technology, such an
+          aspiration is very much possible.
         </p>
         <button class="btn btn-primary">Get Started</button>
       </div>
