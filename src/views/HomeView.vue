ca63897217797ce1cdd7aa022c3c43cb3a689a7f
Hermann EHO <hermannleboss@gmail.com>
2023-11-17 11:56:57 +0000

    feat: blog section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@ import TeamComponent from '@/components/TeamComponent.vue'
 import TimelineComponent from '@/components/TimelineComponent.vue'
 import ProjectComponent from '@/components/ProjectComponent.vue'
 import AboutComponent from '@/components/AboutComponent.vue'
+import BlogSection from '@/components/BlogSection.vue'
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import AboutComponent from '@/components/AboutComponent.vue'
     <TeamComponent />
     <TimelineComponent />
     <ProjectComponent />
+    <BlogSection />
   </main>
 </template>
 
