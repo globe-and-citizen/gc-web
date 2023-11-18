58e1e81278004a954f2da1c9d48a23814d937635
Hermann EHO <hermannleboss@gmail.com>
2023-11-18 10:11:39 +0000

    feat: remove unused component in home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,6 @@
 import HeroComponent from '@/components/HeroComponent.vue'
 import TeamComponent from '@/components/TeamComponent.vue'
 import TimelineComponent from '@/components/TimelineComponent.vue'
-import ProjectComponent from '@/components/ProjectComponent.vue'
 import AboutComponent from '@/components/AboutComponent.vue'
 import BlogSection from '@/components/BlogSection.vue'
 </script>
@@ -13,7 +12,6 @@ import BlogSection from '@/components/BlogSection.vue'
     <AboutComponent />
     <TeamComponent />
     <TimelineComponent />
-    <ProjectComponent />
     <BlogSection />
   </main>
 </template>
