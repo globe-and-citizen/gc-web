4e2444dd139390dbaf044fc7d6fabe0e167c8687
Hermann EHO <hermannleboss@gmail.com>
2023-11-18 10:00:26 +0000

    feat: timeline styled

--- src/components/TimelineComponent.vue
+++ src/components/TimelineComponent.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-col text-center w-full mb-20">
-      <h2 class="text-5xl font-bold">Timeline</h2>
-      <p class="lg:w-2/3 mx-auto leading-relaxed py-8">
-        Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke
-        farm-to-table. Franzen you probably haven't heard of them.
-      </p>
-    </div>
+  <div class="bg-black text-white rounded-t-full flex flex-col text-center w-full mb-20 p-10">
+    <h2 class="text-5xl font-bold">Timeline</h2>
+    <p class="lg:w-2/3 mx-auto leading-relaxed py-8">
+      Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke
+      farm-to-table. Franzen you probably haven't heard of them.
+    </p>
+  </div>
+  <div class="container px-5 mx-auto">
     <ul
       class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical max-w-4xl m-auto"
     >
