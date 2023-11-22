c50dd8967369da401e57d0a90205bf09d68e695d
Hermann EHO <hermannleboss@gmail.com>
2023-11-22 06:48:51 +0000

    feat: fetch blog articles

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -4,21 +4,17 @@
       class="card border border-black rounded-lg overflow-hidden"
       style="box-shadow: 2px 2px 0 0 #000"
     >
-      <img
-        class="lg:h-48 md:h-36 w-full object-cover object-center"
-        :src="article.image"
-        alt="blog"
-      />
+      <img class="w-full object-contain object-center" :src="article.cover_image" alt="blog" />
       <div class="p-6 card-body">
         <p class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
           {{ article.category }}
         </p>
-        <h3 class="card-title">{{ article.title }}</h3>
+        <h3 class="card-title line-clamp-2">{{ article.title }}</h3>
         <p class="leading-relaxed mb-3">
           {{ article.description }}
         </p>
         <div class="flex items-center flex-wrap">
-          <a class="btn btn-outline"
+          <a class="btn btn-outline" :href="article.url"
             >Learn More
             <svg
               class="w-4 h-4 ml-2"
@@ -34,22 +30,8 @@
             </svg>
           </a>
           <span
-            class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200"
+            class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1"
           >
-            <svg
-              class="w-4 h-4 mr-1"
-              stroke="currentColor"
-              stroke-width="2"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              viewBox="0 0 24 24"
-            >
-              <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-              <circle cx="12" cy="12" r="3"></circle></svg
-            >1.2K
-          </span>
-          <span class="text-gray-400 inline-flex items-center leading-none text-sm">
             <svg
               class="w-4 h-4 mr-1"
               stroke="currentColor"
@@ -62,7 +44,7 @@
               <path
                 d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"
               ></path></svg
-            >6
+            >{{ article.comments_count }}
           </span>
         </div>
       </div>
@@ -75,6 +57,9 @@ interface Article {
   description: string
   image: string
   title: string
+  url: string
+  comments_count: number
+  cover_image: string
 }
 
 interface Props {
