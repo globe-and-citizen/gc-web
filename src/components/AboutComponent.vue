de491334b2c53cb499ea25e1cee1a8dd2f05a3c7
Hermann EHO <hermannleboss@gmail.com>
2023-11-26 17:46:36 +0000

    feat: update about content

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -1,4 +1,5 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
   <div class="hero border-b-2 border-black" id="about">
@@ -7,36 +8,30 @@
         <h2 class="text-5xl font-bold">Globe & Citizen: News of Our World</h2>
         <h3>What</h3>
         <p>
-          Globe & Citizen the company is a fully remote company operated from locations spread
-          around the world. Our goal is to innovate and iterate until we are Web3.0’s premier
-          distributed news network. Global issues such as pandemics, supply chain entanglement, and
-          climate change, to name only a few, cannot be solved by any one nation acting alone.
-          Therefore, we have set out to create a global forum that is free from singular regional
-          interests. Leveraging the incredible advances in information technology, such an
-          aspiration is very much possible.
+          Globe & Citizen is a fully remote company operated from locations spread around the world. Our goal is to
+          innovate and iterate until we are Web3.0’s premier distributed news network. We are not a DAO. Rather, we are
+          organizing as the world’s first Crypto Native Corporation.
         </p>
-        <h3>Globe & Citizen the Website</h3>
+        <h3>Why</h3>
         <p>
-          The team at Globe & Citizen is hard at work laying the foundations for Globe & Citizen in
-          the form of Celebrity Fanalyzer, a celebrity gossip blog. The goal is to create a platform
-          where open source ranking algorithms can be developed, and chosen, by users.
-          Simultaneously, we aim to develop a framework for algorithmic compensation using crypto
-          that incentivizes independent journalists and regional news agencies to publish and
-          participate on our platform. The technology necessary to do this already exists. The
-          challenge is compensating those authors who make the best contributions. Of course, what
-          is the definition of best? To help answer this question and define, “what is the best
-          content,” Globe & Citizen is to become the world’s first Anthroscope – an invented term
-          for what we are trying to build.
-          <br />
-          An Anthroscope, like any other scope (telescope microscope, oscilloscope), is designed to
-          give its users the capacity to view something with a new perspective. To function, an
-          anthroscope will leverage modern statistical analysis and big data to produce clever
-          visuals reflecting the unbiased opinion scape of any users viewing any particular piece of
-          content. If done correctly, an anthroscope should have the power to make obvious the
-          inherent biases of a viewer by reflecting back to them who they are and where they fit in
-          the opinion scape. From this new perspective, a rational individuals should be able to
-          infer their own truth. To be fair, Anthroscopes already exist. For example, look at your
-          YouTube home page. It is not populated at random. It is a reflection of you.
+          Global concerns such as pandemics, inflation, and climate change, to name only a few, are issues that cannot
+          be solved by any one nation acting alone. Therefore, we have set out to create a global forum that is free
+          from singular or centralized regional interests. Leveraging the incredible advances in information technology
+          and the Web3.0 space, such an aspiration is very much possible.
+        </p>
+        <h3>How</h3>
+        <p>
+          The team is working hard to lay the foundations for Globe & Citizen in the form of Celebrity Fanalyzer, a celebrity gossip blog. The goal is threefold:
+          <br>
+          <ol>
+            <li>To create a platform where opensource ranking algorithms can be developed, and chosen, by users.
+            </li>
+            <li>Make accessible the fully anonymized metadata associated with any particular piece of content allowing users to see an unbiased “opinion-scape.”
+            </li>
+            <li>Develop a framework for algorithmic compensation to incentivize independent journalists and regional news agencies to publish and participate on our platform.
+            </li>
+          </ol>
+          The technology necessary to accomplish these three goals already exists. The challenge is in execution. Using Celebrity Fanalyzer as a learning platform, we are hard at work trying to make Globe & Citizen a reality.
         </p>
       </div>
     </div>
