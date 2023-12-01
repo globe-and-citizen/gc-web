d4ae00d97f513325b81ea15aaabe286999076721
Hermann EHO <hermannleboss@gmail.com>
2023-12-01 10:11:06 +0000

    feat: small content for about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div role="alert" class="alert">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+      <span>Wellcome to our easteregg page</span>
+    </div>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
 }
 </style>
