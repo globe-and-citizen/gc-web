97afa0fefa4d92f8497f12f14270cfe541475803
Hermann EHO <hermannleboss@gmail.com>
2023-12-01 13:10:57 +0000

    feat: easter-egg page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="about">
+  <div class="">
     <h1>This is an about page</h1>
-    <div role="alert" class="alert">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-      <span>Wellcome to our easteregg page</span>
+    <div class="container mx-auto">
+      <div role="alert" class="alert">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+        <span>Wellcome to our easter-egg page</span>
+      </div>
     </div>
   </div>
 </template>
@@ -12,3 +14,5 @@
 @media (min-width: 1024px) {
 }
 </style>
+<script setup lang="ts">
+</script>
