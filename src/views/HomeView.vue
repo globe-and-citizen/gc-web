ceafac949bfd045a67c668dcd891df17bf287bb1
Hermann EHO <hermannleboss@gmail.com>
2023-12-01 10:10:48 +0000

    feat: create  global call function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,12 @@ import TeamComponent from '@/components/TeamComponent.vue'
 import TimelineComponent from '@/components/TimelineComponent.vue'
 import AboutComponent from '@/components/AboutComponent.vue'
 import BlogSection from '@/components/BlogSection.vue'
+// import app store
+import { useAppStore } from '@/stores/app'
+import router from '@/router'
+
+const appStore=useAppStore()
+console.log("Call the function Called in the console to see the easteregg")
 
 declare global {
   interface Window {
@@ -13,6 +19,10 @@ declare global {
 
 const called = () : void =>{
   console.log("i'm called")
+  appStore.call()
+
+  // Redirect to about page
+ router.push({ name: 'about' })
 }
 window.called = called;
 </script>
