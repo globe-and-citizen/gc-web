3b68dec806f7aa3e612042540a435595c133b24f
Hermann EHO <hermannleboss@gmail.com>
2023-12-01 09:18:00 +0000

    feat: expose called function into windows global

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,17 @@ import TeamComponent from '@/components/TeamComponent.vue'
 import TimelineComponent from '@/components/TimelineComponent.vue'
 import AboutComponent from '@/components/AboutComponent.vue'
 import BlogSection from '@/components/BlogSection.vue'
+
+declare global {
+  interface Window {
+    called: () => void;
+  }
+}
+
+const called = () : void =>{
+  console.log("i'm called")
+}
+window.called = called;
 </script>
 
 <template>
