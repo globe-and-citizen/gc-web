e63f111aafe60cd4c0ea70bcef426e1decbe8168
Hermann EHO <hermannleboss@gmail.com>
2023-12-10 10:40:24 +0000

    feat: set images

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,46 +1,15 @@
 <template>
   <div class="">
-    <section class="text-gray-600 body-font">
-      <div class="container px-5 py-24 mx-auto flex flex-wrap">
-        <div class="flex w-full mb-20 flex-wrap">
-          <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4">Master Cleanse
-            Reliac Heirloom</h1>
-          <p class="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon
-            brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them
-            man bun deep jianbing selfies heirloom.</p>
-        </div>
-        <div class="flex flex-wrap md:-m-2 -m-1">
-          <div class="flex flex-wrap w-1/2">
-            <div class="md:p-2 p-1 w-1/2">
-              <img alt="gallery" class="w-full object-cover h-full object-center block"
-                   src="https://dummyimage.com/500x300">
-            </div>
-            <div class="md:p-2 p-1 w-1/2">
-              <img alt="gallery" class="w-full object-cover h-full object-center block"
-                   src="https://dummyimage.com/501x301">
-            </div>
-            <div class="md:p-2 p-1 w-full">
-              <img alt="gallery" class="w-full h-full object-cover object-center block"
-                   src="https://dummyimage.com/600x360">
-            </div>
-          </div>
-          <div class="flex flex-wrap w-1/2">
-            <div class="md:p-2 p-1 w-full">
-              <img alt="gallery" class="w-full h-full object-cover object-center block"
-                   src="https://dummyimage.com/601x361">
-            </div>
-            <div class="md:p-2 p-1 w-1/2">
-              <img alt="gallery" class="w-full object-cover h-full object-center block"
-                   src="https://dummyimage.com/502x302">
-            </div>
-            <div class="md:p-2 p-1 w-1/2">
-              <img alt="gallery" class="w-full object-cover h-full object-center block"
-                   src="https://dummyimage.com/503x303">
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <img src="../../public/assets/art/1_young_man.png" alt="Image" style="width: 100%">
+    <img src="../../public/assets/art/2_old_man.png" alt="Image" style="width: 100%">
+    <img src="../../public/assets/art/3_city.png" alt="Image" style="width: 100%">
+    <img src="../../public/assets/art/4_office.png" alt="Image" style="width: 100%">
+    <img src="../../public/assets/art/5.png" alt="Image" style="width: 100%">
+    <img src="../../public/assets/art/6_Summer_Sky_in_City%201.png" alt="Image" style="width: 100%">
+    <img src="../../public/assets/art/7_woman.png" alt="Image" style="width: 100%">
+    <img src="../../public/assets/art/8_cars.png" alt="Image" style="width: 100%">
+    <img src="../../public/assets/art/9_New_Chapter_Begins.png" alt="Image" style="width: 100%">
+    <img src="../../public/assets/art/10_Road_to_utopia.png" alt="Image" style="width: 100%">
   </div>
 </template>
 
