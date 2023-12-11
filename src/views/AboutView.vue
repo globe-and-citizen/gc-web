bc83416dedbf267c9179bcca47b4f416f9ed303c
Hermann EHO <hermannleboss@gmail.com>
2023-12-11 17:34:34 +0000

    feat: to walking man

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,14 +2,28 @@
   <div class="">
     <div>
       <div class="sticky top-0 h-0">
-        <img src="../../public/assets/art/item/1_old_man.png" alt="Image" class="w-1/4">
+        <img src="/assets/art/item/1_old_man.png" alt="Image" class="w-1/6 ml-auto">
       </div>
 
       <img src="../../public/assets/art/1_young_man.png" alt="Image" style="width: 100%">
       <img src="../../public/assets/art/2_old_man.png" alt="Image" style="width: 100%">
     </div>
-    <img src="../../public/assets/art/3_city.png" alt="Image" style="width: 100%">
-    <img src="../../public/assets/art/4_office.png" alt="Image" style="width: 100%">
+    <div>
+      <div class="sticky top-0 h-0">
+        <img src="/assets/art/item/2_thinking.png" alt="Image" class="w-1/4 mr-auto">
+      </div>
+      <img src="../../public/assets/art/3_city.png" alt="Image" style="width: 100%">
+      <img src="../../public/assets/art/4_office.png" alt="Image" style="width: 100%">
+    </div>
+    <div class="relative mb-10">
+      <div class="sticky top-0">
+        <img src="/assets/art/item/3_walking_man.png" alt="Image" class="w-1/6 ml-auto">
+      </div>
+      <p class="w-1/4 m-auto text-6xl top-0">Here I was. Walking alone in the city. On my way to deal with another job while blasting funk jazz late into the night. Funk jazz had  always kept me going....
+      </p>
+
+    </div>
+
     <img src="../../public/assets/art/5.png" alt="Image" style="width: 100%">
     <img src="../../public/assets/art/6_Summer_Sky_in_City%201.png" alt="Image" style="width: 100%">
     <img src="../../public/assets/art/7_woman.png" alt="Image" style="width: 100%">
