3d8805f15373f10b663b2d5f9ba8cb3b5d1f2123
Hermann EHO <hermannleboss@gmail.com>
2023-12-11 15:54:16 +0000

    feat: first images

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="">
-    <img src="../../public/assets/art/1_young_man.png" alt="Image" style="width: 100%">
-    <img src="../../public/assets/art/2_old_man.png" alt="Image" style="width: 100%">
+    <div>
+      <div class="sticky top-0 h-0">
+        <img src="../../public/assets/art/item/1_old_man.png" alt="Image" class="w-1/4">
+      </div>
+      <img src="../../public/assets/art/1_young_man.png" alt="Image" style="width: 100%">
+      <img src="../../public/assets/art/2_old_man.png" alt="Image" style="width: 100%">
+    </div>
     <img src="../../public/assets/art/3_city.png" alt="Image" style="width: 100%">
     <img src="../../public/assets/art/4_office.png" alt="Image" style="width: 100%">
     <img src="../../public/assets/art/5.png" alt="Image" style="width: 100%">
